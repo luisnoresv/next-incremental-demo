101622fd9ef560c263e3b26b6829fe788bbd0386
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2023-06-26 20:03:16 -0500

    feat(dogs): fix style for on hover state

--- src/components/dogs/DogsList.tsx
+++ src/components/dogs/DogsList.tsx
@@ -10,11 +10,11 @@ export default function DogsList({ dogs }: Props) {
   return (
     <ul className='flex flex-col'>
       {dogs.map((dog) => (
-        <li className='p-4 border-b-2 border-solid border-purple-700 text-slate-700 hover:bg-purple-800 hover:text-white' key={dog.id}>
+        <li className='group p-4 border-b-2 border-solid border-purple-700 text-slate-700 hover:bg-purple-800 hover:text-white' key={dog.id}>
           <article>
             <h2 className='font-medium'>{dog.breeds[0].name}</h2>
             <p>
-              <Link className='text-sm font-bold hover:text-yellow-300' href={`/dogs/${dog.id}`}>View Details</Link>
+              <Link className='text-sm font-bold text-purple-500 group-hover:text-yellow-300' href={`/dogs/${dog.id}`}>View Details</Link>
             </p>
           </article>
         </li>
